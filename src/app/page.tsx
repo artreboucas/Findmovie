8a807ecb7834a91217d4cd364cd66b56eb901def
Arthur <arthurreboucas123@gmail.com>
2025-05-08 23:26:53 -0300

    estrutura de pastas

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState } from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>FindMovie 🎥</h1>
+    </main>
+  )
+}
